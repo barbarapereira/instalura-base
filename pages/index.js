e732db136ca3aa54fa51d075b2cf4a5173d7841d
Barbara Pereira <barbara.pereira1990@gmail.com>
2021-08-02 14:25:23 -0300

    subindo projeto

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+/* 
+const Title = styled.h1`
+  font-size: 50px;
+  color: ${({ theme }) => theme.colors.primary};
+` */
+function Title(props) {
+  return <h1>{props.children}</h1>
+}
+
+export default function Home() {
+  return <div><Title>Outro título</Title></div>
+}
