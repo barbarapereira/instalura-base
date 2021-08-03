970a5aeb3b55b2b87f18b4f0340b48d134e6ddba
Barbara Pereira <barbara.pereira1990@gmail.com>
2021-08-03 17:24:51 -0300

    criando componente MenuWrapper e separando os arquivos

---
+++ pages/src/components/commons/Menu/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { MenuWrapper } from './styles/MenuWrapper';
+
+export default function Menu() {
+  return (
+    <MenuWrapper>
+      <div>
+        Logo Area (esquerda)
+      </div>
+      <div>
+        Links Area (centro)
+      </div>
+      <div>
+        Buttons Area (direita)
+      </div>
+    </MenuWrapper>
+  )
+}
